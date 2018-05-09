72fdf905db1fb9ce476464fd0eb3f5ff10a66e4c
Allison <allison.cook@ocsbstudent.ca>
2018-05-09 12:52:01 -0400

    Initial commit

---
+++ MinValue/MinValueForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MinValue
+{
+    partial class frmMinValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(98, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(24, 228);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(149, 24);
+            this.lblMin.TabIndex = 1;
+            this.lblMin.Text = "The min value is:";
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 24;
+            this.lstNumbers.Location = new System.Drawing.Point(79, 53);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 172);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // frmMinValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMinValue";
+            this.Text = "Finding the Min value by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.ListBox lstNumbers;
+    }
+}
+
---
+++ MinValue/MinValueForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 9 May, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Find Min Value
+ * This program finds the min value out of ten randomly generated numbers.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinValue
+{
+    public partial class frmMinValue : Form
+    {
+        public frmMinValue()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
